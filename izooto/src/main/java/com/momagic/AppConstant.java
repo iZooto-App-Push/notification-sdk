f2ae7715f6f368db60e1c16b8293f425b02aa943
Amit <amit@datability.co>
2021-06-03 11:39:21 +0530

    Add the Xiaomi and Huawei condition

--- izooto/src/main/java/com/momagic/AppConstant.java
+++ izooto/src/main/java/com/momagic/AppConstant.java
@@ -258,7 +258,7 @@ public interface AppConstant {
     String AD_TYPE="tp";
     String NOTIFICATION_MESSAGE="Send Notification";
     String YES="yes";
-    String SDKVERSION="1.0.3";
+    String SDKVERSION="1.0.4";
     String QSDK_VERSION="&av=";
     String SDK="SDKVERSION";
     String PACKAGE_NAME="&mpn=";
